18aba04b9002b4598e5bf78a406a5ce3c3e61936
sworc <lightbox@nifty.com>
2022-05-18 16:17:15 +0900

    Add files via upload

--- access-1.php
+++ access-1.php
@@ -3,18 +3,24 @@ session_cache_limiter('nocache');
 session_start();
 header( "Content-Type: text/html; Charset=UTF-8" );
 
+// 日本人が PHP 使う前提条件
 mb_language('Japanese');
 mb_internal_encoding('UTF-8');
 
+// DSN : データソースネーム
 $dbh = new PDO("odbc:H-ACCDB");
 
+// キャラクタセット変換
 $sql = mb_convert_encoding("SELECT * FROM 社員マスタ", "SJIS", "UTF-8");
 
 $stmt = $dbh->prepare($sql);
 $stmt->execute();
 
+// fetch は引数なし
 while( $rows = $stmt->fetch() ){
-    print mb_convert_encoding($rows[1], "UTF-8", "SJIS") . "<br>";
+    print mb_convert_encoding($rows[0], "UTF-8", "SJIS") . " / ";
+    print mb_convert_encoding($rows[1], "UTF-8", "SJIS") . " / ";
+    print mb_convert_encoding($rows[2], "UTF-8", "SJIS") . "<br>";
 }
 
 ?>
